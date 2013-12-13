16d46ee35f0ce2c4431b5e055d1065e47cf68d5f
Adam Flanagan <ady8288@icloud.com>
2013-12-13 17:45:36 +0000

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mango::Application.config.secret_token = '262f1530cbd4be1fa97bdf23c610a16e493324ea241317fe877409289fdf9e1ec88c85cb9b5e337a253102b899a00c84d51bde4e24fedbabbc01528e2bd16dc8'
